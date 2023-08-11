f25f5c9e2ca3bd888cfde1049fe72a7f9fffe4df
Guido W. Pettinari <coccoinomane@gmail.com>
2023-08-11 17:00:28 +0200

    🧪 Fixed compound deploy scripts > list markets

--- scripts/deploy_ceth.py
+++ scripts/deploy_ceth.py
@@ -1,17 +1,17 @@
-#!/usr/bin/python3
-
-from sys import argv
-
 from ape import accounts, project
 
 
 def main() -> None:
     """Deploy Compound cETH money market"""
+
     alice = accounts.test_accounts[0]
+
     comptroller = project.Comptroller.deploy(sender=alice)
+
     ir_model = project.WhitePaperInterestRateModel.deploy(
         0, 200000000000000000, sender=alice
     )
+
     ceth = project.CEther.deploy(
         comptroller,
         ir_model,
@@ -22,9 +22,5 @@ def main() -> None:
         alice,
         sender=alice,
     )
-    # If everything went fine, we need to raise an exception to allow
-    # the user to interact with the contracts in interactive mode
-    if "-I" in argv or "--interactive" in argv:
-        raise Exception(
-            "Script completed successfully, now allowing you to use interactive mode"
-        )
+
+    comptroller._supportMarket(ceth, sender=alice)
